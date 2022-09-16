a2843de5efe05c56cef397d44d3aba514de09a83
Tamer Zreg <tamer.zreg@stu.oscb.ca>
2022-09-16 03:43:36 +0000

    Added helloWorldstyle.cpp

---
+++ helloWorldstyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Tamer Zreg All rights reservedcpplint helloWorldStyle.cpp
+//
+// Created by: Tamer Zreg
+// Date: Sep. 24, 2022
+// This program displays "Hello, World! with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! with style" << std::endl;
+    std::cout << "C++ is harder to learn than Python" << std::endl;
+}
